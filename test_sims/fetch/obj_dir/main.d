fe101be09750145a41bec287120575db3e70eb6c
tom <tfarmer7980@gmail.com>
2026-03-30 17:23:10 +0100

    Remove more unneeded files

--- test_sims/fetch/obj_dir/main.d
+++
@@ -1,6 +0,0 @@
-main.o: ../main.cpp Vinstruction_fetch.h \
- /home/tom/projects/oss-cad-suite/share/verilator/include/verilated.h \
- /home/tom/projects/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/tom/projects/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/tom/projects/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/tom/projects/oss-cad-suite/share/verilator/include/verilated_funcs.h
